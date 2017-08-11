e177d843440624e5cff694e737a48f34ae453ce0
Eric Jiang <ejiang@amazon.com>
2017-08-11 14:48:38 -0700

    Add unit test.

--- src/test/java/me/ericjiang/settlers/data/game/GameDaoPostgresIntegrationTest.java
+++ src/test/java/me/ericjiang/settlers/data/game/GameDaoPostgresIntegrationTest.java
@@ -7,8 +7,10 @@ import org.junit.Test;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
+import me.ericjiang.settlers.core.actions.Action;
 import me.ericjiang.settlers.core.game.Game;
 import me.ericjiang.settlers.core.game.GameFactory;
+import me.ericjiang.settlers.core.player.Player;
 import me.ericjiang.settlers.data.board.BoardDao;
 import me.ericjiang.settlers.data.board.BoardDaoInMemory;
 import me.ericjiang.settlers.data.player.PlayerDao;
@@ -37,9 +39,20 @@ public class GameDaoPostgresIntegrationTest {
 
     @Test
     public void shouldRetrieveGamesWeStoreInReverseOrder() {
-        String name = "foo";
-        Game game = gameDao.createGame(name, GameFactory.BASE);
+        gameDao.createGame("foo1", GameFactory.BASE);
+        Game game2 = gameDao.createGame("foo2", GameFactory.BASE);
         List<Game> games = gameDao.openGames();
-        assertEquals(game, games.get(0));
+        assertEquals(game2, games.get(0));
+    }
+
+    @Test
+    public void shouldCreateGameWithDaos() {
+        Game game = gameDao.createGame("foo", GameFactory.BASE);
+        game.connectPlayer(new Player() {
+            public void update(Action action) {}
+            public String id() {
+                return "";
+            }
+        });
     }
 }
