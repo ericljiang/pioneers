7765da33ea91c92228b4f3783e53d67a76685deb
Eric Jiang <ejiang@amazon.com>
2017-08-17 11:30:40 -0700

    Add test for serializing Color enum.

--- src/test/java/me/ericjiang/settlers/core/actions/ActionTest.java
+++ src/test/java/me/ericjiang/settlers/core/actions/ActionTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import me.ericjiang.settlers.core.game.Game.Color;
+
 public class ActionTest {
 
     @Test
@@ -16,5 +18,13 @@ public class ActionTest {
     public void shouldDeserializeWhatWeSerialize() {
         Action action = new ConnectAction("id", "name");
         assertEquals(action, Action.valueOf(action.toString()));
+    }
+
+    @Test
+    public void shouldSerializeAndDeserializeColor() {
+        Action joinAction = new JoinAction("id", "name", Color.BLUE);
+        assertEquals(Color.BLUE, ((JoinAction) Action.valueOf(joinAction.toString())).getColor());
+        Action leaveAction = new LeaveAction("id", "name", Color.BLUE);
+        assertEquals(Color.BLUE, ((JoinAction) Action.valueOf(leaveAction.toString())).getColor());
     }
 }
