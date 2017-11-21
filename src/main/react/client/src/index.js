e6ad1df5a8c004607f0d74ec53ad3a969efd6375
Eric Jiang <ejiang@amazon.com>
2017-11-20 17:51:42 -0800

    Refactor Google One-Tap.

--- src/main/react/client/src/index.js
+++ src/main/react/client/src/index.js
@@ -9,7 +9,7 @@ registerServiceWorker();
 
 window.onGoogleYoloLoad = (googleyolo) => {
     // The 'googleyolo' object is ready for use.
-    const retrievePromise = window.googleyolo.retrieve({
+    const retrievePromise = googleyolo.retrieve({
         supportedAuthMethods: [
             "https://accounts.google.com"
         ],
@@ -42,17 +42,57 @@ window.onGoogleYoloLoad = (googleyolo) => {
         // you can call `hint()` to prompt the user to select an account to sign
         // in or sign up with.
         if (error.type === 'noCredentialsAvailable') {
-            const hintPromise = window.googleyolo.hint({
-                supportedAuthMethods: [
-                    "https://accounts.google.com"
-                ],
-                supportedIdTokenProviders: [
-                    {
-                        uri: "https://accounts.google.com",
-                        clientId: "224119011410-5hbr37e370ieevfk9t64v9799kivttan.apps.googleusercontent.com"
-                    }
-                ]
-            });
+            hint(googleyolo);
         }
     });
 };
+
+function hint(googleyolo) {
+    const hintPromise = googleyolo.hint({
+        supportedAuthMethods: [
+            "https://accounts.google.com"
+        ],
+        supportedIdTokenProviders: [
+            {
+                uri: "https://accounts.google.com",
+                clientId: "224119011410-5hbr37e370ieevfk9t64v9799kivttan.apps.googleusercontent.com"
+            }
+        ]
+    });
+    hintPromise.then((credential) => {
+        if (credential.idToken) {
+            // Send the token to your auth backend.
+            // useGoogleIdTokenForAuth(credential.idToken);
+        }
+    }, (error) => {
+        console.log(error);
+        switch (error.type) {
+            case "userCanceled":
+                // The user closed the hint selector. Depending on the desired UX,
+                // request manual sign up or do nothing.
+                break;
+            case "noCredentialsAvailable":
+                // No hint available for the session. Depending on the desired UX,
+                // request manual sign up or do nothing.
+                break;
+            case "requestFailed":
+                // The request failed, most likely because of a timeout.
+                // You can retry another time if necessary.
+                break;
+            case "operationCanceled":
+                // The operation was programmatically canceled, do nothing.
+                break;
+            case "illegalConcurrentRequest":
+                // Another operation is pending, this one was aborted.
+                break;
+            case "initializationError":
+                // Failed to initialize. Refer to error.message for debugging.
+                break;
+            case "configurationError":
+                // Configuration error. Refer to error.message for debugging.
+                break;
+            default:
+            // Unknown error, do nothing.
+        }
+    });
+}
