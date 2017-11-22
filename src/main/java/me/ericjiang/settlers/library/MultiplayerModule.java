93829319b3926c94243957a40d74e61511ecf8f0
Eric Jiang <ejiang@amazon.com>
2017-11-21 20:39:16 -0800

    Enable subtyping of Events.

--- src/main/java/me/ericjiang/settlers/library/MultiplayerModule.java
+++ src/main/java/me/ericjiang/settlers/library/MultiplayerModule.java
@@ -3,7 +3,11 @@ package me.ericjiang.settlers.library;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Multimap;
+import com.google.common.collect.Sets;
+
+import java.util.Collection;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Consumer;
 
 public abstract class MultiplayerModule {
@@ -38,11 +42,25 @@ public abstract class MultiplayerModule {
 
     @SuppressWarnings("unchecked")
     public <T extends Event> void handleEvent(T event) {
-        eventHandlers.get(event.getClass())
-                .forEach(handler -> handler.accept(event));
+        getAllSuperclasses(event.getClass()).forEach(eventType -> {
+            eventHandlers.get(eventType).forEach(handler -> {
+                handler.accept(event);
+            });
+        });
     }
 
     public <T extends Event> void on(Class<T> eventType, Consumer<T> consumer) {
         eventHandlers.put(eventType, consumer);
+    }
+
+    @SuppressWarnings("unchecked")
+    private Collection<Class<? extends Event>> getAllSuperclasses(Class<? extends Event> eventType) {
+        Set<Class<? extends Event>> classes = Sets.newHashSet(eventType);
+        Class<?> clazz = eventType;
+        while (clazz.equals(Event.class)) {
+            clazz = clazz.getSuperclass();
+            classes.add((Class<? extends Event>) clazz);
+        }
+        return classes;
     }
 }
