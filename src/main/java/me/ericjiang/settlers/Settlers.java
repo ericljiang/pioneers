fbe726b606cd196ae13a949211ff36079c4f0e05
Eric Jiang <ejiang@amazon.com>
2017-11-22 21:25:15 -0800

    Add react-router.

--- src/main/java/me/ericjiang/settlers/Settlers.java
+++ src/main/java/me/ericjiang/settlers/Settlers.java
@@ -3,7 +3,7 @@ package me.ericjiang.settlers;
 import static spark.Spark.*;
 
 import com.google.gson.Gson;
-
+import java.io.IOException;
 import me.ericjiang.settlers.library.game.GameDao;
 import me.ericjiang.settlers.library.game.GameFactory;
 import me.ericjiang.settlers.library.game.GameWebSocketRouter;
@@ -12,11 +12,14 @@ import me.ericjiang.settlers.library.lobby.LobbyWebSocketHandler;
 import me.ericjiang.settlers.simple.SimpleGame;
 import me.ericjiang.settlers.simple.SimpleGameDao;
 import me.ericjiang.settlers.simple.SimpleGameFactory;
+import spark.Spark;
+import spark.utils.IOUtils;
 
 public class Settlers {
 
     public Settlers(
             Gson gson,
+            String client,
             LobbyWebSocketHandler lobbyWebSocketHandler,
             GameWebSocketRouter gameWebSocketRouter) {
 
@@ -26,16 +29,22 @@ public class Settlers {
         webSocket("/ws/game", gameWebSocketRouter);
 
         get("/api/hello", (req, res) -> new Greeting(), gson::toJson);
+
+        // Routes in react-router
+        get("/lobby", (req, res) -> client);
+        get("/game", (req, res) -> client);
+        get("/message", (req, res) -> client);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Gson gson = new Gson();
+        String client = IOUtils.toString(Spark.class.getResourceAsStream("/public/index.html"));
         GameFactory<SimpleGame> gameFactory = new SimpleGameFactory();
         GameDao<SimpleGame> gameDao = new SimpleGameDao();
         Lobby<SimpleGame> lobby = new Lobby<SimpleGame>(gameFactory, gameDao);
         LobbyWebSocketHandler lobbyWebSocketHandler = new LobbyWebSocketHandler(lobby);
         GameWebSocketRouter gameWebSocketRouter = new GameWebSocketRouter(lobby);
 
-        new Settlers(gson, lobbyWebSocketHandler, gameWebSocketRouter);
+        new Settlers(gson, client, lobbyWebSocketHandler, gameWebSocketRouter);
     }
 }
--- src/main/react/client/src/App.js
+++ src/main/react/client/src/App.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 import getGreeting from './settlersClient.js';
@@ -7,17 +13,20 @@ import { lobby } from './lobby.js';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Message />
-        <Lobby />
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <Link to="/message">Message</Link>
+          <Route exact path="/" render={() => (
+            <Redirect to="/lobby" />
+          )} />
+          <Route path="/message" component={Message} />
+          <Route path="/lobby" component={Lobby} />
+        </div>
+      </Router>
     );
   }
 }
@@ -55,7 +64,7 @@ class Lobby extends Component {
       <div>
         <CreateGameForm />
         {Object.entries(this.state.games).map(gameEntry =>
-          <p>{gameEntry[0]} {gameEntry[1].name}</p>
+          <Game id={gameEntry[0]} game={gameEntry[1]} />
         )}
       </div>
     );
@@ -84,6 +93,15 @@ class CreateGameForm extends Component {
       </form>
     )
   }
+}
+
+function Game(props) {
+  return (
+    <div>
+      {props.id} {props.game.name}
+      <Link to="/message">Play</Link>
+    </div>
+  );
 }
 
 export default App;
