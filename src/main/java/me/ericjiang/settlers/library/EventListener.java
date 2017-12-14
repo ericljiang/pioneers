645c91cce3f965c168c6a7a743f7868c26193995
Eric Jiang <ejiang@amazon.com>
2017-12-14 11:09:22 -0800

    Ensure event handlers execute in the order in which they are registered.

--- src/main/java/me/ericjiang/settlers/library/EventListener.java
+++ src/main/java/me/ericjiang/settlers/library/EventListener.java
@@ -1,39 +1,44 @@
 package me.ericjiang.settlers.library;
 
 import com.google.common.collect.LinkedListMultimap;
-import com.google.common.collect.Multimap;
 import com.google.common.collect.Sets;
-import java.util.Collection;
+
 import java.util.Set;
 import java.util.function.Consumer;
 
 public class EventListener {
 
     @SuppressWarnings("rawtypes")
-    private final transient Multimap<Class<? extends Event>, Consumer> eventHandlers;
+    private final transient LinkedListMultimap<Class<? extends Event>, Consumer> eventHandlers;
 
     public EventListener() {
         eventHandlers = LinkedListMultimap.create();
     }
 
+    /**
+     * Process event with each registered handler in registration order
+     */
     @SuppressWarnings("unchecked")
     public <T extends Event> void handleEvent(T event) {
-        getAllSuperclasses(event.getClass()).forEach(eventType -> {
-            eventHandlers.get(eventType).forEach(handler -> {
-                handler.accept(event);
-            });
-        });
+        Set<Class<? extends Event>> eventTypes = getAllSuperclasses(event.getClass());
+        eventHandlers.entries().stream()
+                .filter(entry -> eventTypes.contains(entry.getKey()))
+                .map(entry -> entry.getValue())
+                .forEach(handler -> handler.accept(event));
     }
 
+    /**
+     * Register an event handler
+     */
     public <T extends Event> void on(Class<T> eventType, Consumer<T> consumer) {
         eventHandlers.put(eventType, consumer);
     }
 
     @SuppressWarnings("unchecked")
-    private Collection<Class<? extends Event>> getAllSuperclasses(Class<? extends Event> eventType) {
+    private Set<Class<? extends Event>> getAllSuperclasses(Class<? extends Event> eventType) {
         Set<Class<? extends Event>> classes = Sets.newHashSet(eventType);
         Class<?> clazz = eventType;
-        while (clazz.equals(Event.class)) {
+        while (!clazz.equals(Event.class)) {
             clazz = clazz.getSuperclass();
             classes.add((Class<? extends Event>) clazz);
         }
