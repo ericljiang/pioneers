49b2b7050902e20a5b97067cf915370a9fdc3f1f
Eric Jiang <ejiang@amazon.com>
2018-03-09 12:08:36 -0800

    Use GoogleAuthenticator instead of MockAuthenticator.

--- src/main/java/me/ericjiang/frontiersmen/Frontiersmen.java
+++ src/main/java/me/ericjiang/frontiersmen/Frontiersmen.java
@@ -10,10 +10,9 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+import java.util.Optional;
 import me.ericjiang.frontiersmen.library.auth.Authenticator;
-// import me.ericjiang.frontiersmen.library.auth.GoogleAuthenticator;
-import me.ericjiang.frontiersmen.library.auth.MockAuthenticator;
+import me.ericjiang.frontiersmen.library.auth.GoogleAuthenticator;
 import me.ericjiang.frontiersmen.library.game.GameDao;
 import me.ericjiang.frontiersmen.library.game.GameDaoPostgres;
 import me.ericjiang.frontiersmen.library.game.GameFactory;
@@ -27,6 +26,14 @@ import me.ericjiang.frontiersmen.simple.SimpleGameFactory;
 
 @Slf4j
 public class Frontiersmen {
+
+    private static final String HEROKU_PORT_VARIABLE = "PORT";
+
+    private static final String HEROKU_DATABASE_URL_VARIABLE = "JDBC_DATABASE_URL";
+
+    private static final int DEFAULT_PORT = 4567;
+
+    private static final String DEFAULT_DATABASE_URL = "jdbc:postgresql:frontiersmen";
 
     public Frontiersmen(
             int port,
@@ -53,8 +60,7 @@ public class Frontiersmen {
         GameFactory gameFactory = new SimpleGameFactory(gameDao);
         PlayerRepository playerRepository = new InMemoryPlayerRepository();
         Lobby lobby = new Lobby(gameFactory, playerRepository);
-        // Authenticator authenticator = new GoogleAuthenticator();
-        Authenticator authenticator = new MockAuthenticator();
+        Authenticator authenticator = new GoogleAuthenticator();
         LobbyWebSocketHandler lobbyWebSocketHandler = new LobbyWebSocketHandler(lobby, authenticator, playerRepository);
         GameWebSocketRouter gameWebSocketRouter = new GameWebSocketRouter(lobby, authenticator, playerRepository);
 
@@ -65,17 +71,20 @@ public class Frontiersmen {
         new Frontiersmen(port, gson, lobbyWebSocketHandler, gameWebSocketRouter);
     }
 
-    private static Connection getDatabaseConnection() throws SQLException {
-        String dbUrl = System.getenv("JDBC_DATABASE_URL");
-        dbUrl = (dbUrl != null) ? dbUrl : "jdbc:postgresql:frontiersmen";
-        Connection connection = DriverManager.getConnection(dbUrl);
-        log.info("Connected to database at " + dbUrl);
-        return connection;
+    /**
+     * Return default port if Heroku port isn't set (i.e. on localhost)
+     */
+    private static int getPort() {
+        return Optional.ofNullable(System.getenv(HEROKU_PORT_VARIABLE))
+                .map(s -> Integer.parseInt(s))
+                .orElse(DEFAULT_PORT);
     }
 
-    private static int getPort() {
-        String port = System.getenv("PORT");
-        // return default port if heroku-port isn't set (i.e. on localhost)
-        return port != null ? Integer.parseInt(port) : 4567;
+    private static Connection getDatabaseConnection() throws SQLException {
+        String databaseUrl = Optional.ofNullable(System.getenv(HEROKU_DATABASE_URL_VARIABLE))
+                .orElse(DEFAULT_DATABASE_URL);
+        Connection connection = DriverManager.getConnection(databaseUrl);
+        log.info("Connected to database at " + databaseUrl);
+        return connection;
     }
 }
