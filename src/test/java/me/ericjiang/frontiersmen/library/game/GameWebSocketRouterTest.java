15828e3a086e7f0f50862f48eb55a9cbe3869446
Eric Jiang <ejiang@amazon.com>
2018-03-13 11:16:51 -0700

    Add unit test for GameWebSocketRouter.

---
+++ src/test/java/me/ericjiang/frontiersmen/library/game/GameWebSocketRouterTest.java
@@ -0,0 +1,43 @@
+package me.ericjiang.frontiersmen.library.game;
+
+import static org.easymock.EasyMock.*;
+
+import java.util.Optional;
+
+import org.easymock.EasyMockSupport;
+import org.eclipse.jetty.websocket.api.Session;
+import org.junit.Before;
+import org.junit.Test;
+
+import me.ericjiang.frontiersmen.library.auth.Authenticator;
+import me.ericjiang.frontiersmen.library.lobby.Lobby;
+import me.ericjiang.frontiersmen.library.player.PlayerRepository;
+
+public class GameWebSocketRouterTest extends EasyMockSupport {
+
+    private Lobby lobby;
+
+    private Authenticator authenticator;
+
+    private PlayerRepository playerRepository;
+
+    private GameWebSocketRouter gameWebSocketRouter;
+
+    @Before
+    public void before() {
+        this.lobby = createNiceMock(Lobby.class);
+        this.authenticator = createNiceMock(Authenticator.class);
+        this.playerRepository = createNiceMock(PlayerRepository.class);
+        this.gameWebSocketRouter = new GameWebSocketRouter(lobby, authenticator, playerRepository);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionForNonexistantGameId() {
+        Session session = createNiceMock(Session.class);
+
+        expect(lobby.getGame(anyString())).andReturn(Optional.empty());
+        replayAll();
+
+        gameWebSocketRouter.getModule(session);
+    }
+}
