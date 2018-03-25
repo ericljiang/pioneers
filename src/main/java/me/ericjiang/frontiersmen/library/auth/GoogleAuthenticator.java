797d7dab0a1a3fbfa2df7cbe7c674894f6bcbf10
Eric Jiang <ejiang@amazon.com>
2018-03-24 23:43:08 -0700

    Add unit test for GoogleAuthenticator.

--- src/main/java/me/ericjiang/frontiersmen/library/auth/GoogleAuthenticator.java
+++ src/main/java/me/ericjiang/frontiersmen/library/auth/GoogleAuthenticator.java
@@ -2,26 +2,16 @@ package me.ericjiang.frontiersmen.library.auth;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
+
+import lombok.AllArgsConstructor;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.util.Collections;
 
+@AllArgsConstructor
 public class GoogleAuthenticator implements Authenticator {
 
     private final GoogleIdTokenVerifier verifier;
-
-    private static final String CLIENT_ID = "224119011410-5hbr37e370ieevfk9t64v9799kivttan.apps.googleusercontent.com";
-
-    public GoogleAuthenticator() {
-        NetHttpTransport transport = new NetHttpTransport();
-        JacksonFactory jsonFactory = JacksonFactory.getDefaultInstance();
-        this.verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
-            .setAudience(Collections.singletonList(CLIENT_ID))
-            .build();
-    }
 
     @Override
     public String verify(String playerId, String authToken) throws GeneralSecurityException {
