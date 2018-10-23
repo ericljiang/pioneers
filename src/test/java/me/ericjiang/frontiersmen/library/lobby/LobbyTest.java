6bf0c2449843868c51e9191380b573498b6c79ec
Eric Jiang <eric.jiang2@gmail.com>
2018-10-23 16:42:52 -0400

    Move input validation from Events to handlers of those Events. (#96)
    * Revert "Validate player names and game names."
    
    This reverts commit 1ff7bf005e4a042e1800ef87dd1746353e634952.
    
    * Move input validation from Events to handlers of those Events.

--- src/test/java/me/ericjiang/frontiersmen/library/lobby/LobbyTest.java
+++ src/test/java/me/ericjiang/frontiersmen/library/lobby/LobbyTest.java
@@ -14,16 +14,17 @@ import org.junit.Test;
 import me.ericjiang.frontiersmen.library.game.Game;
 import me.ericjiang.frontiersmen.library.game.GameFactory;
 import me.ericjiang.frontiersmen.library.player.Player;
+import me.ericjiang.frontiersmen.library.player.PlayerNameChangeEvent;
 import me.ericjiang.frontiersmen.library.player.PlayerRepository;
 import me.ericjiang.frontiersmen.library.pregame.Pregame;
 import me.ericjiang.frontiersmen.library.pregame.StartGameEvent;
 
-public class LobbyIntegrationTest extends EasyMockSupport {
+public class LobbyTest extends EasyMockSupport {
 
     private static final String GAME_ID = "1";
     private static final String NAME = "name";
-    private static final String CREATOR_ID = "1";
-    private static final Player CREATOR = new Player(CREATOR_ID);
+    private static final String PLAYER_ID = "1";
+    private static final Player CREATOR = new Player(PLAYER_ID);
     private static final int MINIMUM_PLAYERS = 0;
     private static final int MAXIMUM_PLAYERS = 10;
     private static final Map<String, Object> ATTRIBUTES = Maps.newHashMap();
@@ -54,22 +55,56 @@ public class LobbyIntegrationTest extends EasyMockSupport {
 
         recordExpectations(game);
         expect(gameFactory.loadGames()).andStubReturn(Maps.newHashMap());
-        expect(gameFactory.createPregame(NAME, CREATOR_ID, ATTRIBUTES)).andStubReturn(pregame);
+        expect(gameFactory.createPregame(NAME, PLAYER_ID, ATTRIBUTES)).andStubReturn(pregame);
         replayAll();
 
         GameCreationEvent gameCreationEvent = new GameCreationEvent(NAME, ATTRIBUTES);
-        gameCreationEvent.setPlayerId(CREATOR_ID);
+        gameCreationEvent.setPlayerId(PLAYER_ID);
         lobby.handleEvent(gameCreationEvent);
         assertTrue(lobby.getPregames().containsKey(GAME_ID));
         assertEquals(pregame, lobby.getPregame(GAME_ID).get());
 
         StartGameEvent startGameEvent = new StartGameEvent();
-        startGameEvent.setPlayerId(CREATOR_ID);
+        startGameEvent.setPlayerId(PLAYER_ID);
         pregame.handleEvent(startGameEvent);
         assertFalse(lobby.getPregames().containsKey(GAME_ID));
         assertTrue(lobby.getGames().containsKey(GAME_ID));
         assertEquals(game, lobby.getGame(GAME_ID).get());
         verifyAll();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotAllowEmptyPlayerName() {
+        PlayerNameChangeEvent playerNameChangeEvent = new PlayerNameChangeEvent(PLAYER_ID, "");
+        lobby.handleEvent(playerNameChangeEvent);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotAllowLongPlayerName() {
+        String name = "";
+        for (int i = 0; i <= 32; i++) {
+            name += "a";
+        }
+        PlayerNameChangeEvent playerNameChangeEvent = new PlayerNameChangeEvent(PLAYER_ID, name);
+        lobby.handleEvent(playerNameChangeEvent);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotAllowEmptyGameName() {
+        GameCreationEvent gameCreationEvent = new GameCreationEvent("", Maps.newHashMap());
+        gameCreationEvent.setPlayerId(PLAYER_ID);
+        lobby.handleEvent(gameCreationEvent);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotAllowLongGameName() {
+        String name = "";
+        for (int i = 0; i <= 32; i++) {
+            name += "a";
+        }
+        GameCreationEvent gameCreationEvent = new GameCreationEvent(name, Maps.newHashMap());
+        gameCreationEvent.setPlayerId(PLAYER_ID);
+        lobby.handleEvent(gameCreationEvent);
     }
 
     private void recordExpectations(Game game) {
